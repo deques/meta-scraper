590b0465c3b82e568d145b6aca23e68017e1950b
Wai Wong <daomen@gmail.com>
2023-03-03 16:05:55 +0100

    Loop through all the pages

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -12,21 +12,35 @@ credentials = {
 
 with requests.session() as s:
     s.post(login, data=credentials)
-    result = s.get(url)
-    doc = BeautifulSoup(result.text, "html.parser")
-    giveaways = doc.body.find_all("div", class_="structItem--giveaway")
+    startPage = s.get(url)
 
-    for giveaway in giveaways:
-        prizes = giveaway.find_all("span")
-        #print(giveaway['data-author'])
-        #print(prizes)
+    # Get the giveaway page
+    doc = BeautifulSoup(startPage.text, "html.parser")
 
-        if len(prizes) == 2:
-            i = 1
-        else:
-            i = 0
-        
-        prize = prizes[i].text.strip()
-        print(giveaway['data-author'] + " - " + prize)
+    # Get the number of pages
+    numPages = doc.body.find_all("li", class_="pageNav-page")
+    num = int(numPages[4].text.strip())
+    i = 1
+
+    # Loop through all the pages
+    while i <= num:
+        givePage = s.get(url + "?page=" + str(i))
+        giveDoc = BeautifulSoup(givePage.text, "html.parser")
+
+        giveaways = giveDoc.body.find_all("div", class_="structItem--giveaway")
+
+        for giveaway in giveaways:
+            prizes = giveaway.find_all("span")
+            #print(giveaway['data-author'])
+            #print(prizes)
+
+            if len(prizes) == 2:
+                index = 1
+            else:
+                index = 0
+            
+            prize = prizes[index].text.strip()
+            print(giveaway['data-author'] + " - " + prize)
+        i = i + 1
     #    giver = giveaway.find("div", class_="structItem-title")
     #    print(giver.text)
